6ccab972d845533716035f6b68c90d9aacdf0146
unknown <atilla.kalayy@gmail.com>
2023-02-26 19:15:46 +0300

    Typescript Class 2nd Part has been completed.

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -7,18 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularapp';
-  name = "Atilla";
-  isUser = true;
+}
 
+class Product {
+  //Fields
+  name: string
+  price: number
+  category: string
+  IsPublish: boolean
 
-  constructor() {
-    //backtick
-    console.log(`Name: ${this.name}`)
-    console.log(`Is User: ${this.isUser}`)
+  //Constructor
+  constructor(name: string, price: number, category: string, IsPublish: boolean) {
+    this.name = name
+    this.price = price
+    this.category = category
+    this.IsPublish = IsPublish
   }
 
-  public Method1() {
-    let name2: string = "Atilla";
+
+  public Topla(x: number, y: number): number {
+    return x + y
+  }
+}
+
+class Product2 extends Product {
+
+  constructor(name: string, price: number, category: string, IsPublish: boolean) {
+    super(name, price, category, IsPublish)
   }
 
+  public Method2() {
+    var product = new Product2("Kalem1", 25, "Kalemler", false)
+  }
 }
