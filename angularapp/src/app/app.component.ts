dcb32514b24b6c7ff0ae1a70935de65699b3e961
unknown <atilla.kalayy@gmail.com>
2023-02-26 18:36:35 +0300

    Typescript Type-1 course has been completed.

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularapp';
+  name: string = "Atilla";
+  isUser: boolean = true;
+  createdDate: Date = new Date();
+  names: string[] = ["Atilla", "Salih", "Veysel"]
+  isUsers: boolean[] = [true, false, true]
+  type: any = 5.65;
+type1:unknown[]=["Ahmet",5,true,new Date()]
+
+
+Method1(){
+  this.type.toFixed();
+}
+
 }
