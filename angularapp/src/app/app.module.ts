ca6ce637f7f066e0e01f1a32feb22b9d0917ca34
unknown <atilla.kalayy@gmail.com>
2023-02-27 17:58:35 +0300

    Component Click Event-1 course has been completed.

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventClickComponent } from './pages/event-click/event-click.component';
 import { ExampleComponent } from './pages/example/example.component';
 import { Example2Component } from './pages/example2/example2.component';
 import { Example3Component } from './pages/example3/example3.component';
@@ -12,7 +13,8 @@ import { Example3Component } from './pages/example3/example3.component';
     AppComponent,
     ExampleComponent,
     Example2Component,
-    Example3Component
+    Example3Component,
+    EventClickComponent
   ],
   imports: [
     BrowserModule,
---
+++ angularapp/src/app/pages/event-click/event-click.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-click',
+  templateUrl: './event-click.component.html',
+  styleUrls: ['./event-click.component.css']
+})
+export class EventClickComponent {
+
+  public writeToConsole():void {
+    console.log("click")
+  }
+
+}
