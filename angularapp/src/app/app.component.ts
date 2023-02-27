e2f9f3debb3177bdf62e8ef939641f190e37150d
unknown <atilla.kalayy@gmail.com>
2023-02-27 17:04:26 +0300

    The course of creating components with Angular CLI has been completed.

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -8,17 +8,3 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angularapp';
 }
-
-interface Product {
-  Name: string
-  Price: Number
-}
-
-class car {
-
-  kalem: Product = { Name: "Kalem1", Price: 250 }
-
-  constructor(kalem: Product) {
-    this.kalem = { Name: "Kalem1", Price: 250 }
-  }
-}
