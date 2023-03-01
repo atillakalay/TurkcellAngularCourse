ecb781c000abca46cf4ce074dd9a8566188383b3
unknown <atilla.kalayy@gmail.com>
2023-03-01 18:24:43 +0300

    NgFor-2 course completed.

--- angularapp/src/app/directive/ngfor/ngfor.component.ts
+++ angularapp/src/app/directive/ngfor/ngfor.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-ngfor',
@@ -7,7 +8,16 @@ import { Component } from '@angular/core';
 })
 export class NgforComponent {
 
-  names:string[]=["Atilla","Atilla","Atilla"]
+  names: string[] = ["Atilla", "Atilla", "Atilla"]
 
+  userList: User[] = []
+
+  constructor() {
+    this.userList.push(new User(1, "Atilla", "atilla@gmail.com"))
+    this.userList.push(new User(2, "Atilla2", "atilla2@gmail.com"))
+    this.userList.push(new User(3, "Atilla3", "atilla3@gmail.com"))
+    this.userList.push(new User(4, "Atilla4", "atilla4@gmail.com"))
+    this.userList.push(new User(5, "Atilla5", "atilla5@gmail.com"))
+  }
 
 }
