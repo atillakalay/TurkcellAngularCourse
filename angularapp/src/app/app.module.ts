9f65164a0705a39a8e6148f9ad3dfb1a0392ce2a
unknown <atilla.kalayy@gmail.com>
2023-03-01 20:06:23 +0300

    NgClass Directive course completed.

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { FormsModule } from '@angular/forms';
 import { NgforComponent } from './directive/ngfor/ngfor.component';
 import { NgIfComponent } from './directive/ng-if/ng-if.component';
 import { NgSwitchComponent } from './directive/ng-switch/ng-switch.component';
+import { NgClassComponent } from './directive/ng-class/ng-class.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgforComponent,
     NgIfComponent,
-    NgSwitchComponent
+    NgSwitchComponent,
+    NgClassComponent
   ],
   imports: [
     BrowserModule,
---
+++ angularapp/src/app/directive/ng-class/ng-class.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-class',
+  templateUrl: './ng-class.component.html',
+  styleUrls: ['./ng-class.component.css']
+})
+export class NgClassComponent {
+
+}
