690eda2928a872fb05d15cd3b523ab063402b4d2
atillakalay <atilla.kalayy@gmail.com>
2023-03-03 16:04:27 +0300

    Template Driven Forms Part 1 course has been completed.

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { TemplateDrivenComponent } from './forms/template-driven/template-driven.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    TemplateDrivenComponent,
   ],
   imports: [
     BrowserModule,
---
+++ angularapp/src/app/forms/template-driven/template-driven.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template-driven',
+  templateUrl: './template-driven.component.html',
+  styleUrls: ['./template-driven.component.css']
+})
+export class TemplateDrivenComponent {
+
+  save(form:NgForm){
+console.log(form.value)
+  }
+}
