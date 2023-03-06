31c73dfc65564987ad95baeca0d7cb0e5cedcb26
atillakalay <atilla.kalayy@gmail.com>
2023-03-06 23:08:17 +0300

    Reactive Forms Part 1 course completed.

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TemplateDrivenComponent } from './forms/template-driven/template-driven.component';
+import { ReactiveComponent } from './forms/reactive/reactive.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateDrivenComponent,
+    ReactiveComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
