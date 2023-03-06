b653a5648d0b3a92a76a0220228ac541ba6e3f65
atillakalay <atilla.kalayy@gmail.com>
2023-03-06 23:58:04 +0300

    Reactive Forms Part 7 (DropDown Menu) course has been completed.

--- angularapp/src/app/forms/reactive/reactive.component.ts
+++ angularapp/src/app/forms/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CountryDropMenu } from 'src/app/models/country-drop-menu';
 
 @Component({
   selector: 'app-reactive',
@@ -9,12 +10,14 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class ReactiveComponent {
 
   loginForm: FormGroup
+  countryMenu: CountryDropMenu[] = [{ value: 0, text: "Seçiniz" }, { value: 1, text: "Türkiye" }, { value: 2, text: "Almanya" }, { value: 3, text: "Fransa" }, { value: 4, text: "Amerika" }]
 
   constructor(fb: FormBuilder) {
     this.loginForm = fb.group({
       userName: '',
       password: '',
-      rememberMe: false
+      rememberMe: false,
+      country:0
     })
   }
 
---
+++ angularapp/src/app/models/country-drop-menu.ts
@@ -0,0 +1,4 @@
+export interface CountryDropMenu {
+  text:string
+  value:number
+}
