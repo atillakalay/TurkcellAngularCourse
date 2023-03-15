56aa7cc28a80664e336f24ccc5f8babb3d048175
atillakalay <atilla.kalayy@gmail.com>
2023-03-15 13:54:32 +0300

    The Transformation Operators (MergeMap Operator) course has been completed.

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { defer, distinct, filter, find, first, from, fromEvent, interval, last, map, mapTo, of, range, single, skip, skipUntil, skipWhile, take, takeWhile, timer } from 'rxjs';
+import { defer, distinct, filter, find, first, from, fromEvent, interval, last, map, mapTo, mergeMap, of, range, single, skip, skipUntil, skipWhile, take, takeWhile, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,13 +15,20 @@ export class AppComponent implements OnInit {
 
 
     const click = fromEvent(document, 'click')
-    click.pipe(mapTo("Merhaba")).subscribe(x=>{console.log(x)})
+    click.pipe(mapTo("Merhaba")).subscribe(x => { console.log(x) })
 
     const names = from([
       { name: "Ahmet", surname: "Yıldırım", email: "ahmet@gmail.com" },
       { name: "Mehmet", surname: "Şükran", email: "mehmet@gmail.com" },
       { name: "Hasan", surname: "Yılmaz", email: "hasan@gmail.com" },
     ])
+
+    var stringArray = of("a", "b", "b", "c", "d")
+    var numberArray = of(1, 2, 3, 4, 5)
+
+    stringArray.pipe(mergeMap(x => numberArray.pipe(map(y => x + y)))).subscribe(x => {
+      console.log(x)
+    })
 
     names.pipe(mapTo("Sabit Değer")).subscribe(x => {
       console.log(x)
