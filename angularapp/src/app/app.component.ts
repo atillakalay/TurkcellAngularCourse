3f821b4c5d7d94163a648ae809609bb9f888d55c
atillakalay <atilla.kalayy@gmail.com>
2023-03-16 16:35:33 +0300

    The HttpClient Service (GET) Part 4 tutorial has been completed.

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { User } from './models/user';
-import { User2 } from './models/user2';
+import { Post } from './models/post';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +8,15 @@ import { User2 } from './models/user2';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  user: User | undefined
-  user2: User2 | undefined
+  postList:Post[]=[]
   title = 'angularapp';
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
 
-    this.http.get<User2>('https://jsonplaceholder.typicode.com/todos/1').subscribe(x => {
-      this.user2 = x
+    this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts').subscribe(x => {
+      this.postList = x
     })
   }
 
