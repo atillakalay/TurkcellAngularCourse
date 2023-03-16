3e8975eff00e13f7e39b78beb807b4d77bc4ca6c
atillakalay <atilla.kalayy@gmail.com>
2023-03-16 15:47:42 +0300

    HttpClient Service (GET) Part 1 tutorial has been completed.

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,6 +1,5 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ajax } from 'rxjs/ajax';
-import { concat, concatMap, defer, delay, distinct, filter, find, first, from, fromEvent, interval, last, map, mapTo, mergeMap, of, range, reduce, retry, single, skip, skipUntil, skipWhile, switchMap, take, takeWhile, tap, timer, toArray } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +9,13 @@ import { concat, concatMap, defer, delay, distinct, filter, find, first, from, f
 export class AppComponent implements OnInit {
   title = 'angularapp';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
 
-    ajax.getJSON('https://jsonplaceholder.typicode.com/todos/1').pipe(retry(3)).subscribe(x=>{
+    this.http.get('https://jsonplaceholder.typicode.com/todos/1').subscribe(x=>{
       console.log(x)
     })
-
   }
 
 }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FirstComponent } from './first/first.component';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
