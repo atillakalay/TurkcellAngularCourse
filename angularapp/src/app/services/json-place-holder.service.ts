70ca2174576b94d13fc7355543adacb289297e9b
atillakalay <atilla.kalayy@gmail.com>
2023-03-17 20:09:42 +0300

    HttpClient Service | RxJS Operator Example Scenario-2 Part 1 lesson has been completed.

--- angularapp/src/app/services/json-place-holder.service.ts
+++ angularapp/src/app/services/json-place-holder.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { from, map, mergeMap, skip, take, toArray } from 'rxjs';
+import { filter, from, map, mergeMap, skip, take, toArray } from 'rxjs';
 import { Post } from '../models/post';
 
 @Injectable({
@@ -12,10 +12,15 @@ export class JsonPlaceHolderService {
   getPostList(page: number, pageSize: number) {
 
 
-    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts').pipe(mergeMap(x => from(x)), skip((page - 1) * pageSize), take(pageSize),map(x=>`${x.id} ${x.userId}`) ,toArray());
+    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts').pipe(mergeMap(x => from(x)), skip((page - 1) * pageSize), take(pageSize), map(x => `${x.id} ${x.userId}`), toArray());
   }
 
   getPost(id: number) {
     return this.http.get<Post>(`https://jsonplaceholder.typicode.com/posts/${id}`)
   }
+
+  getUserWithSearch(searchText: string) {
+    return this.http.get<any[]>('https://jsonplaceholder.typicode.com/users').pipe(mergeMap(x => from(x)), filter(x => x.name.toLowerCase().include(searchText.toLowerCase())), map(x => x.name))
+  }
+
 }
