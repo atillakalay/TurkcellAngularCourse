e0905a95f2e81918eecb03953ea97cdc3ddb8287
atillakalay <atilla.kalayy@gmail.com>
2023-03-18 15:06:03 +0300

    HttpClient Service | RxJS Operator Example Scenario-3 Part 1 lesson has been completed.

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Post } from './models/post';
 import { JsonPlaceHolderService } from './services/json-place-holder.service';
 
@@ -13,17 +14,16 @@ export class AppComponent implements OnInit {
   isLoadingShow: boolean = true
   postList: string[] = []
   title = 'angularapp';
+  currencyList: string[] = ["Türk Lirası", "Dolar"]
+  selectCurrency = new FormControl("Türk Lirası")
+
 
   constructor(private josnPostPlaceHolderServise: JsonPlaceHolderService) { }
 
   ngOnInit(): void {
-
-    this.josnPostPlaceHolderServise.getPostList(2,5).subscribe(x => {
-      this.postList = x
-      this.isLoadingShow = false
-
+    this.selectCurrency.valueChanges.subscribe(x => {
+      console.log(x)
     })
-
   }
 
 }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { OrnekComponent } from './ornek/ornek.component';
 import { SearchComponent } from './search/search.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AComponent } from './shared/a/a.component';
+import { BComponent } from './shared/b/b.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OrnekComponent,
     SearchComponent,
+    AComponent,
+    BComponent,
   ],
   imports: [
     BrowserModule,
